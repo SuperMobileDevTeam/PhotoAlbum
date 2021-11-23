0e9e64dbb382f5487731a4fbef2000493b43410c
NTL2503 <ngoloc2706@gmail.com>
2021-11-23 14:20:50 +0700

    fragment_Gridview

--- app/src/main/java/com/example/photoalbum/ui/GridViewFragment.java
+++ app/src/main/java/com/example/photoalbum/ui/GridViewFragment.java
@@ -4,9 +4,6 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,25 +13,22 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
+import androidx.fragment.app.Fragment;
 import com.example.photoalbum.R;
-
 import java.util.ArrayList;
+import com.bumptech.glide.Glide;
 
-
-public class AlbumFragment extends Fragment {
+public class GridViewFragment extends Fragment {
     ArrayList<String> images;
-    ArrayList<String> nameAlbum = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        requireActivity().setTitle(R.string.nav_album);
-        View layout = inflater.inflate(R.layout.fragment_album, container, false);
+        requireActivity().setTitle(R.string.nav_gallery);
+        View layout = inflater.inflate(R.layout.fragment_gallery, container, false);
 
-        GridView gridView = (GridView) layout.findViewById(R.id.gridviewAlbum);
-        gridView.setAdapter(new AlbumFragment.ImageAdapter(getActivity(), R.layout.album_item));
+        GridView gridView = (GridView) layout.findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(getActivity()));
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -50,11 +44,9 @@ public class AlbumFragment extends Fragment {
 
     private class ImageAdapter extends BaseAdapter {
         private Activity context;
-        int layout;
 
-        public ImageAdapter(Activity context, int layout){
+        public ImageAdapter(Activity context){
             this.context = context;
-            this.layout = layout;
             images = getAllShownImagePath(context);
         }
 
