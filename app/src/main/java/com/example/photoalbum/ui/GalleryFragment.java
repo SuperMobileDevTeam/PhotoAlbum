b78b86ab8fc467debadc3e2d577dc5a58510fd91
Nhật Huy ★彡 <provcc143300@gmail.com>
2021-12-03 21:19:24 +0700

    Update GalleryFragment.java

--- app/src/main/java/com/example/photoalbum/ui/GalleryFragment.java
+++ app/src/main/java/com/example/photoalbum/ui/GalleryFragment.java
@@ -11,11 +11,93 @@ import android.view.ViewGroup;
 import com.example.photoalbum.R;
 
 public class GalleryFragment extends Fragment {
+    ArrayList<String> images;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         requireActivity().setTitle(R.string.nav_gallery);
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View layout = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        GridView gridView = (GridView) layout.findViewById(R.id.gridviewGallery);
+        gridView.setAdapter(new GalleryFragment.ImageAdapter(getActivity()));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(null != images && !images.isEmpty()) {
+                    Toast.makeText(getActivity(), "Position:" + i + " " + images.get(i),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return layout;
+    }
+
+    private class ImageAdapter extends BaseAdapter {
+        private Activity context;
+
+        public ImageAdapter(Activity context){
+            this.context = context;
+            images = getAllShownImagePath(context);
+        }
+
+        private ArrayList<String> getAllShownImagePath(Activity activity) {
+            Uri uri;
+            Cursor cursor;
+            int column_index_data;
+
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            String[] projection = {
+                    MediaStore.Images.Media.DATA,
+                    MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+            };
+            cursor = activity.getContentResolver().query(uri, projection, null, null, null);
+
+            int count = cursor.getCount();
+            ArrayList<String> listOfAllImages = new ArrayList<>(count);
+            column_index_data = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToPosition(-1);
+            while(cursor.moveToNext()){
+                listOfAllImages.add(cursor.getString(column_index_data));
+            }
+
+            cursor.close();
+            return listOfAllImages;
+        }
+
+        @Override
+        public int getCount() {
+            return images.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return i;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ImageView pictureView;
+            if(view == null){
+                pictureView = new ImageView(context);
+                pictureView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                pictureView.setLayoutParams(new GridView.LayoutParams(270, 270));
+            }
+            else{
+                pictureView = (ImageView) view;
+            }
+
+            Glide.with(context).load(images.get(i))
+                    .placeholder(R.drawable.ic_launcher_foreground)
+                    .centerCrop().into(pictureView);
+
+            return pictureView;
+        }
     }
 }
